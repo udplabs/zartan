3666d10dfcebe339c58a3f3d2e4a6aee3bd0de11
Shawn Recinto <shawn.recinto@gmail.com>
2020-04-22 03:49:54 +0000

    fixed travelagency flake8

--- _travelagency/views.py
+++ _travelagency/views.py
@@ -1,31 +1,21 @@
-import os
-import base64
-import json
-import requests
 import logging
 
-#import functions
-from functools import wraps
-from flask import render_template, url_for, redirect, session,request
-from flask import send_from_directory, make_response
-from flask import Blueprint,g
-from flask import Flask, current_app as app
-from utils.okta import OktaAuth, OktaAdmin, TokenUtil
-from utils.udp import SESSION_INSTANCE_SETTINGS_KEY, get_app_vertical
+# import functions
+from flask import render_template, session
+from flask import Blueprint
+from utils.udp import SESSION_INSTANCE_SETTINGS_KEY
 
 from GlobalBehaviorandComponents.validation import is_authenticated, get_userinfo
 
 logger = logging.getLogger(__name__)
 
-#set blueprint
-travelagency_views_bp = Blueprint('travelagency_views_bp', __name__,template_folder='templates', static_folder='static', static_url_path='static')
+# set blueprint
+travelagency_views_bp = Blueprint('travelagency_views_bp', __name__, template_folder='templates', static_folder='static', static_url_path='static')
 
 
-#Required for Login Landing Page
+# Required for Login Landing Page
 @travelagency_views_bp.route("/profile")
 @is_authenticated
 def travelagency_profile():
     logger.debug("travelagency_profile()")
     return render_template("travelagency/profile.html", user_info=get_userinfo(), config=session[SESSION_INSTANCE_SETTINGS_KEY])
-
-
