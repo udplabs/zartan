9297ec4157b97fcdae5cd5edafd4457cb5ed4573
dzadikdev <ubuntu@ip-172-31-41-78.ec2.internal>
2020-05-28 15:42:07 +0000

    Added Custom Registration for Credit

--- _credit/views.py
+++ _credit/views.py
@@ -2,9 +2,10 @@ import logging
 
 # import functions
 from flask import render_template, session, request
-from flask import Blueprint, redirect
+from flask import Blueprint, redirect, url_for
 from utils.udp import SESSION_INSTANCE_SETTINGS_KEY, get_app_vertical
 from utils.okta import TokenUtil, OktaAdmin
+from utils.email import Email
 
 from GlobalBehaviorandComponents.validation import is_authenticated, get_userinfo
 
@@ -69,3 +70,103 @@ def credit_getmorecredit(app_id):
     app_url = app_info["settings"]["oauthClient"]["initiate_login_uri"]
 
     return redirect(app_url)
+
+@credit_views_bp.route("/registration", methods=["GET"])
+def credit_registration_get():
+    logger.debug("credit_registration_get()")
+    user_info = get_userinfo()
+    return render_template(
+            "{0}/registration.html".format(get_app_vertical()),
+            templatename=get_app_vertical(),
+            config=session[SESSION_INSTANCE_SETTINGS_KEY],
+            user_info=get_userinfo(),
+            _scheme="https")
+            
+@credit_views_bp.route("/registration-state/<stateToken>", methods=["GET"])
+def credit_registration_state_get(stateToken):
+    logger.debug("dealer_registration_state_get()")
+    user_id = stateToken
+    okta_admin = OktaAdmin(session[SESSION_INSTANCE_SETTINGS_KEY])
+    
+    return render_template(
+        "{0}/registration-state.html".format(get_app_vertical()),
+        templatename=get_app_vertical(),
+        userid=user_id,
+        config=session[SESSION_INSTANCE_SETTINGS_KEY],
+        _scheme="https")
+        
+@credit_views_bp.route("/registration-state/<stateToken>", methods=["POST"])
+def credit_registration_state_post(stateToken):
+    logger.debug("dealer_registration_state_get()")
+    user_id = stateToken
+    logger.debug(request.form.get('inputPassword'))
+    okta_admin = OktaAdmin(session[SESSION_INSTANCE_SETTINGS_KEY])
+    user_data = {
+      "credentials": {
+        "password" : { "value": request.form.get('password') }
+        # "recovery_question": {
+        #   "question": 'What is your password?',
+        #   "answer": request.form.get('password')
+        # }
+      }
+    }
+    user_update_response = okta_admin.update_user(user_id=user_id, user=user_data)
+    
+    logger.debug(user_update_response)
+ 
+    if "errorCode" in user_update_response:
+        return render_template(
+            "{0}/registration-state.html".format(get_app_vertical()),
+            userid=user_id,
+            templatename=get_app_vertical(), config=session[SESSION_INSTANCE_SETTINGS_KEY],
+            error=user_update_response['errorCauses'][0]['errorSummary'])
+            
+    user_activate_response = okta_admin.activate_user(user_id, send_email=False)
+    group_info = okta_admin.get_application_groups(session[SESSION_INSTANCE_SETTINGS_KEY]["client_id"])
+    group_id = group_info[0]["id"]
+    user_group_response = okta_admin.assign_user_to_group(group_id, user_id)  
+    message = "Registration Complete! Please Login Now!"
+    return redirect(url_for("gbac_bp.gbac_login", _external="True", _scheme="https", message=message))
+
+
+
+@credit_views_bp.route("/registration-completion", methods=["POST"])
+def credit_registration_completion():
+    logger.debug("credit_registration_completion()")
+
+    user_data = {
+        "profile": {
+            "firstName": request.form.get('firstname'),
+            "lastName": request.form.get('lastname'),
+            "email": request.form.get('email'),
+            "login": request.form.get('email'),
+        }
+    }
+    logger.debug(user_data)
+    okta_admin = OktaAdmin(session[SESSION_INSTANCE_SETTINGS_KEY])
+    user_create_response = okta_admin.create_user(user=user_data, activate_user='false')
+    logger.debug(user_create_response)
+    emailRegistration(
+        recipient={"address": request.form.get('email')},
+        token=user_create_response["id"])
+            
+    return render_template(
+        "{0}/registration-completion.html".format(get_app_vertical()),
+        templatename=get_app_vertical(),
+        config=session[SESSION_INSTANCE_SETTINGS_KEY],
+        _scheme="https")
+        
+  # EMail user and admin when a new user registers successfully
+def emailRegistration(recipient, token):
+        logger.debug("emailRegistration()")
+        app_title = session[SESSION_INSTANCE_SETTINGS_KEY]["settings"]["app_name"]
+        activation_link = url_for("credit_views_bp.credit_registration_state_get", stateToken=token, _external=True, _scheme="https")
+        subject = "Welcome to the {app_title}".format(app_title=session[SESSION_INSTANCE_SETTINGS_KEY]["settings"]["app_name"])
+        # Send Activation Email to the user
+        message = """
+            Thank you for Applying for {app_title}! Click this link to activate your account <br />
+            <a href='{activation_link}'>{activation_link}</a>).
+            """.format(app_title=app_title, activation_link=activation_link)
+        test = Email.send_mail(subject=subject, message=message, recipients=[recipient])
+        logger.debug(test)
+    
