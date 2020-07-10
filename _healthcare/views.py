14d6aea637b77c2c1fc07768f3cde1b75f989640
Dan Zadik <dan.zadik@okta.com>
2020-07-10 14:54:24 +0000

    Replaced ID Proofing in HC Demo with Component

--- _healthcare/views.py
+++ _healthcare/views.py
@@ -1,12 +1,10 @@
 import logging
 import datetime
-import time
 
 from flask import render_template, session, request, redirect, url_for
 from flask import Blueprint
 from utils.udp import SESSION_INSTANCE_SETTINGS_KEY, get_udp_ns_fieldname
-from utils.okta import TokenUtil, OktaAdmin, OktaUtil
-from utils.rest import RestUtil
+from utils.okta import TokenUtil, OktaAdmin
 from GlobalBehaviorandComponents.mfaenrollment import get_enrolled_factors
 
 from GlobalBehaviorandComponents.validation import is_authenticated, get_userinfo
@@ -250,88 +248,6 @@ def healthcare_clear_consent(userid):
             _scheme=session[SESSION_INSTANCE_SETTINGS_KEY]["app_scheme"],
             user_id=userid,
             message=message))
-
-
-@healthcare_views_bp.route("/getverificationcode")
-@is_authenticated
-def healthcare_getverificationcode():
-    logger.debug("healthcare_getverificationcode")
-    user_info = get_userinfo()
-    okta_admin = OktaAdmin(session[SESSION_INSTANCE_SETTINGS_KEY])
-    user = okta_admin.get_user(user_info["sub"])
-    evidenttoken = ""
-
-    basicauth = OktaUtil.get_encoded_auth("okta", "Ry4EZf8SyxKyStLK6BqxBBLXEW4SrIo6hc0m2rR3PoI")
-
-    headers = {
-        "Accept": "application/json",
-        "Content-Type": "application/json",
-        "Authorization": "Basic {0}".format(basicauth)
-    }
-
-    my_str = user["profile"]["email"]
-    idx = my_str.index("@")
-    email = my_str[:idx] + str(time.time()) + my_str[idx:]
-
-    body = {
-        "email": email,
-        "templateId": "1ce55f4e-7bb2-4907-9643-dc61f1f04f4d"
-    }
-
-    response = RestUtil.execute_post(" https://verify.api.demo.evidentid.com/api/v1/verify/requests", headers=headers, body=body)
-    evidenttoken = response["userIdentityToken"]
-    user_data = {"profile": {get_udp_ns_fieldname("evident_id"): response["id"]}}
-    okta_admin.update_user(user["id"], user_data)
-
-    return evidenttoken
-
-
-@healthcare_views_bp.route("/updateidentity")
-@is_authenticated
-def healthcare_updateidentity():
-    logger.debug("healthcare_updateidentity")
-    user_info = get_userinfo()
-    okta_admin = OktaAdmin(session[SESSION_INSTANCE_SETTINGS_KEY])
-    user = okta_admin.get_user(user_info["sub"])
-
-    basicauth = OktaUtil.get_encoded_auth("okta", "Ry4EZf8SyxKyStLK6BqxBBLXEW4SrIo6hc0m2rR3PoI")
-
-    headers = {
-        "Accept": "application/json",
-        "Content-Type": "application/json",
-        "Authorization": "Basic {0}".format(basicauth)
-    }
-
-    evident_id = user["profile"][get_udp_ns_fieldname("evident_id")]
-    response = RestUtil.execute_get("https://verify.api.demo.evidentid.com/api/v1/verify/requests/{0}".format(evident_id), headers=headers)
-
-    user_data = {
-        "profile": {
-            get_udp_ns_fieldname("is_evident_validated"): True
-        }
-    }
-    status = ""
-    for item in response["attributes"]:
-        status = item["status"]
-        if status == "pending":
-            break
-
-        if item["type"] == "core.firstname":
-            user_data["profile"]["firstName"] = item["values"][0]
-        if item["type"] == "core.lastname":
-            user_data["profile"]["lastName"] = item["values"][0]
-        if item["type"] == "core.address.zipcode":
-            user_data["profile"]["zipCode"] = item["values"][0]
-        if item["type"] == "core.address.city":
-            user_data["profile"]["city"] = item["values"][0]
-        if item["type"] == "core.address.state":
-            user_data["profile"]["state"] = item["values"][0]
-    if status == "pending":
-        return status
-    else:
-        okta_admin.update_user(user["id"], user_data)
-
-    return response
 
 
 @healthcare_views_bp.route("/healthrecord")
