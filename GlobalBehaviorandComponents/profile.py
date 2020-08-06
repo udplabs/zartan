a5f8234eeae1c7ec0755a656e18f3f18a2844419
Dan Zadik <dan.zadik@okta.com>
2020-08-06 17:35:58 +0000

    Fix issue with Profile page and Add Refresh Token button

--- GlobalBehaviorandComponents/profile.py
+++ GlobalBehaviorandComponents/profile.py
@@ -1,12 +1,13 @@
 import logging
+import uuid
 
 # import functions
-from flask import render_template, session, request
+from flask import render_template, session, request, redirect
 from flask import Blueprint
-from utils.udp import SESSION_INSTANCE_SETTINGS_KEY, get_app_vertical
-from utils.okta import TokenUtil
+from utils.udp import SESSION_INSTANCE_SETTINGS_KEY, get_app_vertical, apply_remote_config
+from utils.okta import TokenUtil, OktaAuth
 
-from GlobalBehaviorandComponents.validation import is_authenticated, get_userinfo
+from GlobalBehaviorandComponents.validation import is_authenticated, get_userinfo, FROM_URI_KEY
 
 logger = logging.getLogger(__name__)
 
@@ -16,14 +17,35 @@ gbac_profile_bp = Blueprint('gbac_profile_bp', __name__, template_folder='templa
 
 # Required for Login Landing Page
 @gbac_profile_bp.route("/profile")
+@apply_remote_config
 @is_authenticated
 def profile_bp():
-    logger.debug("profile_bp()")
+    logger.debug("profile_bp_profile()")
+    if request.args.get('refreshtoken') == 'true':
+        okta_auth = OktaAuth(session[SESSION_INSTANCE_SETTINGS_KEY])
 
-    return render_template(
-        "/profile.html",
-        templatename=get_app_vertical(),
-        id_token=TokenUtil.get_id_token(request.cookies),
-        access_token=TokenUtil.get_access_token(request.cookies),
-        user_info=get_userinfo(),
-        config=session[SESSION_INSTANCE_SETTINGS_KEY])
+        auth_options = {
+            "response_mode": "form_post",
+            "prompt": "none",
+            "scope": "openid profile email"
+        }
+
+        session["oidc_state"] = str(uuid.uuid4())
+        session[FROM_URI_KEY] = request.url.replace("http://", "{0}://".format(session[SESSION_INSTANCE_SETTINGS_KEY]["app_scheme"])) + "profile"
+
+        oauth_authorize_url = okta_auth.create_oauth_authorize_url(
+            response_type="code",
+            state=session["oidc_state"],
+            auth_options=auth_options
+        )
+
+        return redirect(oauth_authorize_url)
+    else:
+
+        return render_template(
+            "/profile.html",
+            templatename=get_app_vertical(),
+            id_token=TokenUtil.get_id_token(request.cookies),
+            access_token=TokenUtil.get_access_token(request.cookies),
+            user_info=get_userinfo(),
+            config=session[SESSION_INSTANCE_SETTINGS_KEY])
