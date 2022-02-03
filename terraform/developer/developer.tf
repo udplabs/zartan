43fedaacee7d1ef004eb4a6f9fcc01bbbc54f828
Mike Wallick <mike.wallick@okta.com>
2022-02-03 13:21:25 -0600

    Adds post_login_redirect URLs to vertical config

--- terraform/developer/developer.tf
+++ terraform/developer/developer.tf
@@ -25,6 +25,10 @@ resource "okta_app_oauth" "developer" {
     "https://${local.app_domain}/authorization-code/callback",
     "http://localhost:8666/authorization-code/callback"
   ]
+  post_logout_redirect_uris = [
+    "https://${local.app_domain}/index",
+    "http://localhost:8666/index"
+  ]
   response_types = ["code"]
   consent_method = "TRUSTED"
   issuer_mode    = "ORG_URL"
