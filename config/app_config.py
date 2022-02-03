7b84d08970d65f353209c7e507f4c184d11cb2fb
Mike Wallick <mike.wallick@okta.com>
2022-02-03 14:58:28 -0600

    Changes default landing page to index (root of demo app)

--- config/app_config.py
+++ config/app_config.py
@@ -23,7 +23,7 @@ default_settings = {
     "redirect_uri": os.getenv("OKTA_OIDC_REDIRECT_URI", "http://yoursite/authorization-code/callback"),
     "settings": {
         "app_template": os.getenv("APP_TEMPLATE", "sample"),
-        "app_post_login_landing_url": os.getenv("APP_POST_LOGIN_LANDING_URL", "profile"),
+        "app_post_login_landing_url": os.getenv("APP_POST_LOGIN_LANDING_URL", "index"),
         "app_loginmethod": os.getenv("APP_LOGINMETHOD", "standard-widget"),
         "app_name": os.getenv("APP_NAME", "Sample App"),
         "app_slogan": os.getenv("APP_SLOGAN", ""),
